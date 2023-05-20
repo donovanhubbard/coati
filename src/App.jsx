31607db4be99e66a9bc930fab35bec145a1dc0ee
Donovan Hubbard <37090676+donovanhubbard@users.noreply.github.com>
2023-05-19 20:20:07 -0500

    Moving all the logic out of the App component and into it's own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
 
-import LatencyGraph from './components/LatencyGraph';
+import MonitorPanel from './components/MonitorPanel';
 
 const App = () => {
-  const MAX_PINGS = 5;
-  const INTERVAL_MS = 5000;
-
-  const googleObj = {
-    displayName: 'google.com',
-    host: 'google.com',
-    pings: [],
-  }
-
-  const [hosts, setHosts] = useState([googleObj]);
-  const hostsRef = useRef(hosts);
-  hostsRef.current = hosts;
-
-  const sendPing = async (host) => {
-    let response = await window.pingProvider.sendPing(host);
-    const timeString = new Date(Date.now()).toLocaleTimeString();
-    return {...response, formattedTime: timeString};
-  };
-
-  useEffect(() => {
-    const timeout = setInterval(async () => {
-
-      const hostObjs = []
-      
-      for (const hostObj of hostsRef.current){
-        const response = await sendPing(hostObj.host);
-        const newPings = [...hostObj.pings, response];
-        if (newPings.length > MAX_PINGS) {
-          newPings.shift();
-        }
-        const newHostObj = {...hostObj, pings: newPings};
-        hostObjs.push(newHostObj);
-      }
-
-      setHosts(hostObjs);
-    }, INTERVAL_MS);
-    return () => clearInterval(timeout);
-  },[]);
-
-  useEffect(async () => {
-    window.pingProvider.getGateway().then((result) => {
-      const gatewayObj = {
-        displayName: "local gateway",
-        host: result,
-        pings: [],
-      }
-      setHosts([...hosts, gatewayObj]);
-    });
-  },[]);
-
   return (
     <div>
       <h1>Coati</h1>
-      <LatencyGraph hostObjs={hosts} />
+      <MonitorPanel maxPings={5} intervalMs={5000}/>
     </div>
   )
 };
---
+++ src/components/MonitorPanel.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+import LatencyGraph from './LatencyGraph';
+
+const MonitorPanel = ({maxPings, intervalMs}) => {
+
+  const googleObj = {
+    displayName: 'google.com',
+    host: 'google.com',
+    pings: [],
+  }
+
+  const [hosts, setHosts] = useState([googleObj]);
+  const hostsRef = useRef(hosts);
+  hostsRef.current = hosts;
+
+  const sendPing = async (host) => {
+    let response = await window.pingProvider.sendPing(host);
+    const timeString = new Date(Date.now()).toLocaleTimeString();
+    return {...response, formattedTime: timeString};
+  };
+
+  useEffect(() => {
+    const timeout = setInterval(async () => {
+
+      const hostObjs = []
+      
+      for (const hostObj of hostsRef.current){
+        const response = await sendPing(hostObj.host);
+        const newPings = [...hostObj.pings, response];
+        if (newPings.length > maxPings) {
+          newPings.shift();
+        }
+        const newHostObj = {...hostObj, pings: newPings};
+        hostObjs.push(newHostObj);
+      }
+
+      setHosts(hostObjs);
+    }, intervalMs);
+    return () => clearInterval(timeout);
+  },[]);
+
+  useEffect(async () => {
+    window.pingProvider.getGateway().then((result) => {
+      const gatewayObj = {
+        displayName: "local gateway",
+        host: result,
+        pings: [],
+      }
+      setHosts([...hosts, gatewayObj]);
+    });
+  },[]);
+
+  return (
+    <div>
+      <h1>Coati</h1>
+      <LatencyGraph hostObjs={hosts} />
+    </div>
+  )
+};
+
+export default MonitorPanel;
